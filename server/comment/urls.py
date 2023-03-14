dad6c3d1f56f4b6f72a805d11cbdef2a1c8a1964
Ben Song <ben.song@samarkand.global>
2023-03-14 16:55:02 +0800

    rm commemt app

--- server/comment/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import re_path, include
-
-#
-# from . import views
-
-# app_name = "comment"
-urlpatterns = [
-    re_path(r"^comment2/post/(?P<post_pk>[0-9]+)/$", include(""), name="post_comment")
-]
